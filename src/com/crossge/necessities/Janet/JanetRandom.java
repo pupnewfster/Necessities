f4dcf541240a30e22a942072612bb6b5416ec8c3
pupnewfster <richard@freimer.com>
2015-12-21 02:05:42 -0500

    Update them memes

--- src/com/crossge/necessities/Janet/JanetRandom.java
+++ src/com/crossge/necessities/Janet/JanetRandom.java
@@ -15,45 +15,45 @@ public class JanetRandom extends Random {
     };
 
 	public int rInt(int m) {//Returns a random int from 0 - m
-        return (int) ((System.currentTimeMillis() + nextInt()) % m);
+        return m == 0 ? 0 : (int) ((System.currentTimeMillis() + nextInt()) % m);
     }
 
     public int memeRandom(int m) {
-        return (int) Math.sqrt(Math.abs(Math.pow(m, 2)*(pies[rInt(pies.length)].nextInt(m)/Math.PI)))%m;//sqrt((mathExpression/pi)*(applePie)) = meme
+        return m == 0 ? 0 : (int) Math.sqrt(Math.abs(m*(pies[nextInt(pies.length)].nextInt(m)/Math.PI)))%m;//sqrt((mathExpression/pi)*(applePie)) = meme
     }
 
     private static class ApplePie extends Random {
-        @Override
+        /*@Override
         public int nextInt(int m) {//Returns a random int from 0 - m
             return (int) Math.pow(System.currentTimeMillis() + nextInt(), 2);
-        }
+        }*/
     }
 
     private static class GrapePie extends Random {
-        @Override
+        /*@Override
         public int nextInt(int m) {//Returns a random int from 0 - m
             return (int) (System.currentTimeMillis() + nextInt());
-        }
+        }*/
     }
 
     private static class ChocolatePie extends Random {
-        @Override
+        /*@Override
         public int nextInt(int m) {//Returns a random int from 0 - m
             return (int) (Math.pow(System.currentTimeMillis(), 2) + nextInt());
-        }
+        }*/
     }
 
     private static class BlueberryPie extends Random {
         @Override
         public int nextInt(int m) {//Returns a random int from 0 - m
-            return (int) (System.currentTimeMillis() + nextInt());
+            return (int) Math.pow(3.5, 2.6268656716417910447761194029851)*nextInt();
         }
     }
 
     private static class PecanPie extends Random {
-        @Override
+        /*@Override
         public int nextInt(int m) {//Returns a random int from 0 - m
             return (int) System.currentTimeMillis()%m;
-        }
+        }*/
     }
 }
