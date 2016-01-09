3bcd06978425549e434482ba9a3cafa115a8a768
pupnewfster <richard@freimer.com>
2016-01-09 15:02:41 -0500

    Fix showing green dot for online when janet is online

--- src/com/crossge/necessities/Janet/JanetSlack.java
+++ src/com/crossge/necessities/Janet/JanetSlack.java
@@ -249,6 +249,7 @@ public class JanetSlack {
         } catch (Exception e) {
         }
         setUserChannels();
+        sendPost("https://slack.com/api/users.setPresence?token=" + token + "&presence=auto&pretty=1");
         historyReader = new BukkitRunnable() {
             @Override
             public void run() {
@@ -260,7 +261,7 @@ public class JanetSlack {
         keepAlive = new BukkitRunnable() {
             @Override
             public void run() {
-                sendPost("https://slack.com/api/users.setPresence?token=" + token + "&presence=auto&pretty=1");
+                sendPost("https://slack.com/api/users.setActive?token=" + token + "&pretty=1");
             }
         };
         keepAlive.runTaskTimerAsynchronously(Necessities.getInstance(), 0, 30 * 60 * 20);//Every thirty minutes force it to show that janet is still alive
